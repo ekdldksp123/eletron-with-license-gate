56ad012c9c548d58b3a759d55c8c5b993b2a0008
Vinchae Kim <vinchaekim@Vinchaes-MacBook-Pro.local>
2023-02-16 13:19:45 +0900

    :sparkles: add mac address authentication function

--- main.js
+++ main.js
@@ -4,73 +4,95 @@ const { autoUpdater } = require("electron-updater");
 const fetch = require("node-fetch");
 const isDev = process.env.NODE_ENV === "development";
 const licenseKey = require("nodejs-license-key");
+const mac = require("macaddress");
+const dotenv = require("dotenv");
 
-function validateLicenseKey(key) {
+dotenv.config();
+
+function validateLicenseKey(key, mac) {
   const licenseKey = process.env.LICENSE_KEY;
-  if (key === licenseKey) {
+  const macAddress = process.env.MAC_ADDRESS;
+
+  if (key === licenseKey && mac === macAddress) {
     return "VALID";
+  } else {
+    console.log(key, mac);
+    console.log(licenseKey, macAddress);
+    return "INVALID";
   }
-  return "INVALID";
 }
 
 async function gateCreateWindowWithLicense(createWindow) {
-  const gateWindow = new BrowserWindow({
-    resizable: false,
-    frame: false,
-    width: 420,
-    height: 200,
-    webPreferences: {
-      devTools: isDev,
-      preload: path.join(__dirname, "gate.js"),
-    },
-  });
+  const certification = process.env.IS_VALID;
 
-  gateWindow.loadFile("gate.html");
+  console.log("certification : ", certification);
+  if (certification === "valid") createWindow(process.env.LICENSE_KEY);
+  else {
+    const gateWindow = new BrowserWindow({
+      resizable: false,
+      frame: false,
+      width: 420,
+      height: 200,
+      webPreferences: {
+        devTools: isDev,
+        preload: path.join(__dirname, "gate.js"),
+      },
+    });
 
-  if (isDev) {
-    gateWindow.webContents.openDevTools({ mode: "detach" });
-  }
+    gateWindow.loadFile("gate.html");
 
-  const userInfo = {
-    company: "www.pace.com",
-    street: "Taipei 101",
-    city: "Taipei",
-    state: "Taiwan",
-    zip: "100",
-  };
-
-  const userLicense = {
-    info: userInfo,
-    prodCode: "License Test",
-    appVersion: "1.0.0",
-    osType: "IOS",
-  };
-
-  try {
-    const createdLicense = licenseKey.createLicense(userLicense);
-    process.env.LICENSE_KEY = createdLicense.license;
-    console.log(process.env.LICENSE_KEY);
-  } catch (error) {
-    console.log(error);
-  }
-
-  ipcMain.on("GATE_SUBMIT", async (_event, { key }) => {
-    const code = validateLicenseKey(key);
-    console.log(code);
-
-    switch (code) {
-      case "VALID":
-        // Close the license gate window
-        gateWindow.close();
-        // Create our main window
-        createWindow(key);
-        break;
-      default:
-        // Exit the application
-        app.exit(1);
-        break;
+    if (isDev) {
+      gateWindow.webContents.openDevTools({ mode: "detach" });
     }
-  });
+
+    const userInfo = {
+      company: "www.pace.com",
+      street: "Taipei 101",
+      city: "Taipei",
+      state: "Taiwan",
+      zip: "100",
+    };
+
+    const userLicense = {
+      info: userInfo,
+      prodCode: "License Test",
+      appVersion: "1.0.0",
+      osType: "IOS",
+    };
+
+    let macaddress;
+
+    try {
+      macaddress = await mac.one().then((mac) => mac);
+
+      // const createdLicense = licenseKey.createLicense(userLicense);
+      process.env.LICENSE_KEY = process.env.TEMP_LICENSE_KEY;
+      process.env.MAC_ADDRESS = process.env.TEMP_MAC_ADDRESS;
+
+      console.log(process.env.LICENSE_KEY);
+      console.log(process.env.MAC_ADDRESS);
+    } catch (error) {
+      console.log(error);
+    }
+
+    ipcMain.on("GATE_SUBMIT", async (_event, { key }) => {
+      const code = validateLicenseKey(key, macaddress);
+
+      switch (code) {
+        case "VALID":
+          // Close the license gate window
+          gateWindow.close();
+          process.env.IS_VALID = "valid";
+          // Create our main window
+          createWindow(key);
+          break;
+        default:
+          // Exit the application
+          app.exit(1);
+          break;
+      }
+    });
+  }
 }
 
 function createWindow(key) {
